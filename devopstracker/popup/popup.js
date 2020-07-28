42bd3fd1cb171ed85d4e8591856bcc075a0560e0
Aibo Li <aibli@microsoft.com>
2020-07-28 15:53:57 -0700

    update functions

--- devopstracker/popup/popup.js
+++ devopstracker/popup/popup.js
@@ -32,7 +32,10 @@ if (getProjectsBtn) {
 		    	li.setAttribute('id', name);
 		    	li.appendChild(document.createTextNode(name));
 		    	ul.appendChild(li);
-		    }
+			}
+			getWorkItems(function(items) {
+				console.log(items);
+			})
 		}).fail(function(err){
 			alert("Try again champ!");
 		});
@@ -74,4 +77,42 @@ if (getProjectsBtn) {
 
 
 	};
+}
+
+// get the teams information
+function getMyTeams(callback) {
+	$.ajax({
+		type: 'GET',
+		url: 'https://dev.azure.com/microsoft/_apis/teams?api-version=6.0-preview.3&$mine=true',
+		headers: {
+			"Content-Type":"application/json; charset=utf-8;",
+			"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
+		}
+	}).done(function(res) {
+		return callback(res);
+	}).fail(function(err){
+		return callback(err, null);
+	});
+}
+
+// get the teams information
+function getWorkItems(callback) {
+	$.ajax({
+		type: 'POST',
+		// {project}/{team}
+		url: 'https://dev.azure.com/microsoft/ecc7381a-6947-453a-89d7-00907ab894b4/61a6bd9d-6dd4-425b-be60-c4d71b5fd3a8/_apis/wit/wiql?api-version=5.1',
+		headers: {
+			"Content-Type":"application/json; charset=utf-8;",
+			"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
+		},
+		data: JSON.stringify({
+			"query": "Select [System.Id], [System.Title], [System.State] From WorkItems Where [System.State] <> 'Completed' AND [System.State] <> 'Closed' AND [System.AssignedTo] = @me"
+		})
+	}).done(function(res) {
+		console.log(res);
+		return callback(res);
+	}).fail(function(err){
+		console.log(err);
+		return callback(err, null);
+	});
 }
