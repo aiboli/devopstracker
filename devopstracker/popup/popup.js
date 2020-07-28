6499c5804cfa579de1460370e601c2a729426da9
Aibo Li <aibli@microsoft.com>
2020-07-28 14:11:21 -0700

    import jquery

--- devopstracker/popup/popup.js
+++ devopstracker/popup/popup.js
@@ -13,25 +13,37 @@ if (getProjectsBtn) {
 	 //    li.setAttribute('id','testItem1');
 	 //    li.appendChild(document.createTextNode('testItem1'));
 	 //    ul.appendChild(li);
+		$.ajax({
+			type: 'GET',
+			url: 'https://dev.azure.com/microsoft/_apis/projects?api-version=5.1',
+			headers: {
+				"Content-Type":"application/json; charset=utf-8;",
+				"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
+			}
+		}).done(function(res) {
+			// returned json file
+			console.log(res);
+		}).fail(function(err){
+			alert("Try again champ!");
+		});
+	    // var request = new XMLHttpRequest()
 
-	    var request = new XMLHttpRequest()
+		// request.open('GET', 'https://dev.azure.com/microsoft/_apis/projects?api-version=5.1', true)
+		// request.setRequestHeader('Content-Type', 'application/json; charset=utf-8;');
+		// request.setRequestHeader ("Authorization", "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya'));
 
-		request.open('GET', 'https://dev.azure.com/microsoft/_apis/projects?api-version=5.1', true)
-		request.setRequestHeader('Content-Type', 'application/json; charset=utf-8;');
-		request.setRequestHeader ("Authorization", "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya'));
+		// request.onload = function() {
+		//   // Begin accessing JSON data here
 
-		request.onload = function() {
-		  // Begin accessing JSON data here
+		//   var data = JSON.parse(this.response)
 
-		  var data = JSON.parse(this.response)
+		//   if (request.status >= 200 && request.status < 400) {
+		//     console.log(data)
+		//   } else {
+		//     console.log('error')
+		//   }
+		// }
 
-		  if (request.status >= 200 && request.status < 400) {
-		    console.log(data)
-		  } else {
-		    console.log('error')
-		  }
-		}
-
-		request.send()
+		// request.send()
 	};
 }
