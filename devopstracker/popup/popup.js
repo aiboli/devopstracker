b9a3ade0998e2ff69a64db9b0bfecd3bef0f45ec
Tej Suri <gssuri@Tejs-MacBook-Air.local>
2020-07-28 17:47:10 -0400

    ask user for PAT

--- devopstracker/popup/popup.js
+++ devopstracker/popup/popup.js
@@ -5,20 +5,21 @@
 //   };
 // }
 
-const getProjectsBtn = document.getElementById("getbtn");
+var access_token = "";
+
+const getProjectsBtn = document.getElementById("getprojects");
 if (getProjectsBtn) {
 	getProjectsBtn.onclick = function() {
+		if (access_token === "") return;
+
 		var ul = document.getElementById("list");
-		// var li = document.createElement("li");
-	 //    li.setAttribute('id','testItem1');
-	 //    li.appendChild(document.createTextNode('testItem1'));
-	 //    ul.appendChild(li);
+
 		$.ajax({
 			type: 'GET',
 			url: 'https://dev.azure.com/microsoft/_apis/projects?api-version=5.1',
 			headers: {
 				"Content-Type":"application/json; charset=utf-8;",
-				"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
+				"Authorization": "Basic " + btoa('Basic' + ":" + access_token)
 			}
 		}).done(function(res) {
 			// returned json file
@@ -36,42 +37,16 @@ if (getProjectsBtn) {
 		}).fail(function(err){
 			alert("Try again champ!");
 		});
-	    // var request = new XMLHttpRequest()
-
-		// request.open('GET', 'https://dev.azure.com/microsoft/_apis/projects?api-version=5.1', true)
-		// request.setRequestHeader('Content-Type', 'application/json; charset=utf-8;');
-		// request.setRequestHeader ("Authorization", "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya'));
-
-		// request.onload = function() {
-		//   // Begin accessing JSON data here
-
-		//   var data = JSON.parse(this.response)
-
-		//   if (request.status >= 200 && request.status < 400) {
-		//     console.log(data)
-		//   } else {
-		//     console.log('error')
-		//   }
-		// }
-
-		//   if (request.status >= 200 && request.status < 400) {
-		//     //console.log(data)
-		//     var projects = data.value;
-		//     for(i = 0; i < projects.length; i++) {
-		//     	var name = projects[i].name;
-
-		//     	var li = document.createElement("li");
-		//     	li.setAttribute('id', name);
-		//     	li.appendChild(document.createTextNode(name));
-		//     	ul.appendChild(li);
-		//     }
-		//   } else {
-		//     console.log('error')
-		//   }
-		// }
-
-		// request.send()
 
 
 	};
+}
+
+const tokenInput = document.getElementById("token");
+const setTokenBtn = document.getElementById("settoken");
+if (setTokenBtn) {
+	setTokenBtn.onclick = function() {
+		access_token = tokenInput.value;
+		tokenInput.value = '';
+	}
 }
