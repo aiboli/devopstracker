0e78d09c2e0479fc7aedf1b9a4405bdd3a8c79d8
Tej Suri <gssuri@Tejs-MacBook-Air.local>
2020-07-28 19:10:17 -0400

    link all Projects to DevOps URL

--- devopstracker/popup/popup.js
+++ devopstracker/popup/popup.js
@@ -5,7 +5,7 @@
 //   };
 // }
 
-var access_token = "";
+var access_token = "rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya";
 
 const getProjectsBtn = document.getElementById("getprojects");
 if (getProjectsBtn) {
@@ -27,8 +27,7 @@ if (getProjectsBtn) {
 			let data = res;
 			var projects = data.value;
 		    for(i = 0; i < projects.length; i++) {
-		    	var name = projects[i].name;
-		    	var href = "";
+		    	let name = projects[i].name;
 
 		    	$.ajax({
 		    		type: 'GET',
@@ -38,20 +37,23 @@ if (getProjectsBtn) {
 						"Authorization": "Basic " + btoa('Basic' + ":" + access_token)
 					}
 		    	}).done(function(res2) {
-		    		console.log(res2);
-		    		href = res2["_links"]["web"]["href"];
+		    		//console.log(res2["_links"]["web"]["href"]);
+		    		//https://dev.azure.com/microsoft/Base
+		    		//res2["_links"]["web"]["href"]
+
+					var a = document.createElement("a");
+					a.setAttribute('href', res2["_links"]["web"]["href"]);
+					a.setAttribute('target', "_blank");
+					var li = document.createElement("li");
+					li.setAttribute('id', name);
+					li.appendChild(document.createTextNode(name));
+					a.appendChild(li);
+					ul.appendChild(a);
 		    	}).fail(function(err2){
 		    		alert("couldn't get URL");
 		    	});
 
-		    	var a = document.createElement("a");
-				a.setAttribute('href', "https://dev.azure.com/microsoft/Base");
-				a.setAttribute('target', "_blank");
-		    	var li = document.createElement("li");
-		    	li.setAttribute('id', name);
-		    	li.appendChild(document.createTextNode(name));
-		    	a.appendChild(li);
-		    	ul.appendChild(a);
+
 		    }
 		}).fail(function(err){
 			alert("Try again champ!");
