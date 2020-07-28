c6fac58674de1b62b5dfc558707955e1b23843c4
Tej Suri <gssuri@Tejs-MacBook-Air.local>
2020-07-28 18:53:15 -0400

    links

--- devopstracker/popup/popup.js
+++ devopstracker/popup/popup.js
@@ -23,16 +23,35 @@ if (getProjectsBtn) {
 			}
 		}).done(function(res) {
 			// returned json file
-			console.log(res);
+			//console.log(res);
 			let data = res;
 			var projects = data.value;
 		    for(i = 0; i < projects.length; i++) {
 		    	var name = projects[i].name;
+		    	var href = "";
 
+		    	$.ajax({
+		    		type: 'GET',
+		    		url: projects[i].url,
+					headers: {
+						"Content-Type":"application/json; charset=utf-8;",
+						"Authorization": "Basic " + btoa('Basic' + ":" + access_token)
+					}
+		    	}).done(function(res2) {
+		    		console.log(res2);
+		    		href = res2["_links"]["web"]["href"];
+		    	}).fail(function(err2){
+		    		alert("couldn't get URL");
+		    	});
+
+		    	var a = document.createElement("a");
+				a.setAttribute('href', "https://dev.azure.com/microsoft/Base");
+				a.setAttribute('target', "_blank");
 		    	var li = document.createElement("li");
 		    	li.setAttribute('id', name);
 		    	li.appendChild(document.createTextNode(name));
-		    	ul.appendChild(li);
+		    	a.appendChild(li);
+		    	ul.appendChild(a);
 		    }
 		}).fail(function(err){
 			alert("Try again champ!");
