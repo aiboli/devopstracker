8a38531115493397467c5c95fb2ec426bd3f49d3
Aibo Li <aibli@microsoft.com>
2020-07-28 20:50:38 -0700

    fixed some ui

--- devopstracker/background.js
+++ devopstracker/background.js
@@ -50,6 +50,67 @@ chrome.runtime.onMessage.addListener(
         break;
     }
 
-    //return true; 
+    //return true;
   }
 );
+
+// get the teams information
+function getMyTeams(callback) {
+	$.ajax({
+		type: 'GET',
+		url: 'https://dev.azure.com/microsoft/_apis/teams?api-version=6.0-preview.3&$mine=true',
+		headers: {
+			"Content-Type":"application/json; charset=utf-8;",
+			"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
+		}
+	}).done(function(res) {
+		return callback(res);
+	}).fail(function(err) {
+		return callback(err, null);
+	});
+}
+
+// get the teams information
+function getWorkItems(callback) {
+	// send request to query all the work items
+	$.ajax({
+		type: 'POST',
+		url: 'https://dev.azure.com/microsoft/ecc7381a-6947-453a-89d7-00907ab894b4/61a6bd9d-6dd4-425b-be60-c4d71b5fd3a8/_apis/wit/wiql?api-version=5.1',
+		headers: {
+			"Content-Type":"application/json",
+			"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
+		},
+		data: JSON.stringify({
+			"query": "Select [System.Id], [System.Title], [System.State] From WorkItems Where [System.State] <> 'Completed' AND [System.State] <> 'Closed' AND [System.AssignedTo] = @me"
+		})
+	}).done(function(res) {
+		let ids = getWorkItemsList(res);
+		// getting work items details
+		$.ajax({
+			type: 'GET',
+			url: `https://dev.azure.com/microsoft/_apis/wit/workitems?ids=${ids.toString()}&api-version=6.0-preview.3`,
+			headers: {
+				"Content-Type":"application/json",
+				"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
+			},
+		}).done(function(res) {
+			return callback(res);
+		}).fail(function(err){
+			console.log(err);
+			return callback(err, null);
+		})
+	}).fail(function(err) {
+		console.log(err);
+		return callback(err, null);
+	});
+}
+
+// get all the work items
+function getWorkItemsList(data) {
+	let workItems = data.workItems;
+	let ids = [];
+	for (let i = 0; i < workItems.length; i++) {
+		ids.push(workItems[i].id);
+	}
+	return ids;
+}
--- devopstracker/popup/popup.js
+++ devopstracker/popup/popup.js
@@ -25,21 +25,6 @@ if (getProjectsBtn) {
 			console.log(response);
 			if(!response.result || response.result === "error") return;
 			projects = response.result;
-		});
-
-		var ul = document.getElementById("list");
-
-		$.ajax({
-			type: 'GET',
-			url: 'https://dev.azure.com/microsoft/_apis/projects?api-version=5.1',
-			headers: {
-				"Content-Type":"application/json; charset=utf-8;",
-				"Authorization": "Basic " + btoa('Basic' + ":" + access_token)
-			}
-		}).done(function(res) {
-			// returned json file
-			let data = res;
-			var projects = data.value;
 			// sort projects
 			projects.sort(function(a, b) {
 				let a_name = a.name.toUpperCase();
@@ -53,21 +38,23 @@ if (getProjectsBtn) {
 				}
 			});
 			console.log(projects);
-		    for(i = 0; i < projects.length; i++) {
+			for(i = 0; i < projects.length; i++) {
 				let name = projects[i].name;
 				// create project button
-				$('#list').append(createProjectButton(projects[i]));
-		    	// $.ajax({
-		    	// 	type: 'GET',
-		    	// 	url: projects[i].url,
+				$('#list').append(createProjectOption(projects[i]));
+
+
+				// $.ajax({
+				// 	type: 'GET',
+				// 	url: projects[i].url,
 				// 	headers: {
 				// 		"Content-Type":"application/json; charset=utf-8;",
 				// 		"Authorization": "Basic " + btoa('Basic' + ":" + access_token)
 				// 	}
-		    	// }).done(function(res2) {
-		    		//console.log(res2["_links"]["web"]["href"]);
-		    		//https://dev.azure.com/microsoft/Base
-		    		//res2["_links"]["web"]["href"]
+				// }).done(function(res2) {
+					//console.log(res2["_links"]["web"]["href"]);
+					//https://dev.azure.com/microsoft/Base
+					//res2["_links"]["web"]["href"]
 
 					// var a = document.createElement("a");
 					// a.setAttribute('href', res2["_links"]["web"]["href"]);
@@ -77,11 +64,24 @@ if (getProjectsBtn) {
 					// li.appendChild(document.createTextNode(name));
 					// a.appendChild(li);
 					// ul.appendChild(a);
-		    	// }).fail(function(err2){
-		    	// 	alert("couldn't get URL");
+				// }).fail(function(err2){
+				// 	alert("couldn't get URL");
 				// });
 			}
 		});
+
+		var ul = document.getElementById("list");
+
+		// $.ajax({
+		// 	type: 'GET',
+		// 	url: 'https://dev.azure.com/microsoft/_apis/projects?api-version=5.1',
+		// 	headers: {
+		// 		"Content-Type":"application/json; charset=utf-8;",
+		// 		"Authorization": "Basic " + btoa('Basic' + ":" + access_token)
+		// 	}
+		// }).done(function(res) {
+
+		// });
 		// if (access_token === "") {
 		// 	chrome.runtime.sendMessage({cmd: "getToken"}, function(response) {
 		// 		console.log(response);
@@ -110,70 +110,9 @@ if (getProjectsBtn) {
 	};
 }
 
-// get the teams information
-function getMyTeams(callback) {
-	$.ajax({
-		type: 'GET',
-		url: 'https://dev.azure.com/microsoft/_apis/teams?api-version=6.0-preview.3&$mine=true',
-		headers: {
-			"Content-Type":"application/json; charset=utf-8;",
-			"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
-		}
-	}).done(function(res) {
-		return callback(res);
-	}).fail(function(err) {
-		return callback(err, null);
-	});
-}
-
-// get the teams information
-function getWorkItems(callback) {
-	// send request to query all the work items
-	$.ajax({
-		type: 'POST',
-		url: 'https://dev.azure.com/microsoft/ecc7381a-6947-453a-89d7-00907ab894b4/61a6bd9d-6dd4-425b-be60-c4d71b5fd3a8/_apis/wit/wiql?api-version=5.1',
-		headers: {
-			"Content-Type":"application/json",
-			"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
-		},
-		data: JSON.stringify({
-			"query": "Select [System.Id], [System.Title], [System.State] From WorkItems Where [System.State] <> 'Completed' AND [System.State] <> 'Closed' AND [System.AssignedTo] = @me"
-		})
-	}).done(function(res) {
-		let ids = getWorkItemsList(res);
-		// getting work items details
-		$.ajax({
-			type: 'GET',
-			url: `https://dev.azure.com/microsoft/_apis/wit/workitems?ids=${ids.toString()}&api-version=6.0-preview.3`,
-			headers: {
-				"Content-Type":"application/json",
-				"Authorization": "Basic " + btoa('Basic' + ":" + 'rzzq3rpwxygmcetwrtdnu4rigavoeltaboes5vsiewbbucpdq3ya')
-			},
-		}).done(function(res) {
-			return callback(res);
-		}).fail(function(err){
-			console.log(err);
-			return callback(err, null);
-		})
-	}).fail(function(err) {
-		console.log(err);
-		return callback(err, null);
-	});
-}
-
-// get all the work items
-function getWorkItemsList(data) {
-	let workItems = data.workItems;
-	let ids = [];
-	for (let i = 0; i < workItems.length; i++) {
-		ids.push(workItems[i].id);
-	}
-	return ids;
-}
-
 // create button component
-function createProjectButton(project) {
-	let button = `<li><button class='project-button'>${project.name}</button></li>`;
+function createProjectOption(project) {
+	let button = `<option value="${project.id}">${project.name}</option>`;
 	return button;
 }
 
